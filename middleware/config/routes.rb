a4d97c2c1fe0e560d19fdb76e87a76bb24313e02
Gabriel Rael <gabriel.rael@storable.com>
2019-09-20 14:54:03 -0400

    Add metric/:id endpoint to pull last 6 sprint metrics for a team

--- middleware/config/routes.rb
+++ middleware/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'metrics', to: 'metrics#index'
-      get 'metrics/history', to: 'metrics#history'
+      get 'metrics/:id', to: 'metrics#show'
     end
   end
 end
