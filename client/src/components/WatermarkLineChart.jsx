847aef709d3306af035409c19118e8be4fb947ca
Gabriel Rael <gabriel.rael@storable.com>
2019-12-10 12:16:30 -0500

    Add skeleton for WatermarkLineChart component

---
+++ client/src/components/WatermarkLineChart.jsx
@@ -0,0 +1,10 @@
+// import * as d3 from "d3"
+import React from 'react'
+
+export class WatermarkLineChart extends React.Component {
+    render() {
+      return (
+        <div className={this.props.chart}></div>
+      )
+    }
+  }
